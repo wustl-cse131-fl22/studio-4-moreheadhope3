00760ec94e7059ff178734596c40c1f229688d72
benpaoni <benpaoni@bens-mbp.wusm-wifi.wucon.wustl.edu>
2022-10-12 15:45:52 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,6 +7,9 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 
-
+StdDraw.setPenColor(0,255,0);
+StdDraw.filledRectangle(0.4,0.6,0.3,0.2);
+StdDraw.setPenColor(0,0,255);
+StdDraw.filledCircle(0.45, 0.55, 0.1);
 	}
 }
